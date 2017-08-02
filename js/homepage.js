edb3839f424d68aada17e57edd88497e3708e6b4
naveednasib <naveed.nasib@gaditek.com>
2017-08-03 00:13:15 +0500

    adding new fold scrolling fold/partical fold / and 3 folds

--- js/homepage.js
+++ js/homepage.js
@@ -11,66 +11,72 @@ $(document).ready(function () {
   var typed = new Typed(".element", options);
 
 
-  var scroll_circle = $(".scroll-container").offset().top;
+    var scroll_circle = $(".scroll-container").offset().top,
+    headerHeight = $('header').height();
+    var scroll_circle_height = $(".scroll-container").height();
+    var  scroll_circle_bottom = $(".scroll-container").offset().top;
+    scroll_circle_bottom = scroll_circle_height + scroll_circle ;
 
-  $(window).scroll(function () {
+    circleScrolled = 0;
+    lastScroll = 0;
+    var count =0;
+    var wraper_height =  $(".wrapper-holder").height();
+    var tranformVal;
+
+
+
+  $(window).scroll(function (e) {
     particle_parrallax();
+    scrollTop = $(window).scrollTop();
 
 
-    var windowTop = $(window).scrollTop();
+    
+    
+       var positiondown = (scroll_circle - scrollTop - headerHeight);      
+       var positivePositon = (-(positiondown)   );
+       
+      if(positiondown < 1   &&    (positivePositon < scroll_circle_height)  ){          
+          $(".circle-radius").addClass("add-circle");
+            $(".atom-feature").css("margin-bottom",positivePositon);
+            count += 1;           
+           var tranformVal = (count );
+           tranformVal += (tranformVal / (tranformVal*10)) ;
+            $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+(tranformVal)+")"});
+           }    
 
-    if ((scroll_circle - 300)) { }
+      
+          else if(scroll_circle_bottom <=  scrollTop){          
+              console.log(scroll_circle_bottom  +" __ "+  scrollTop);              
+              count -= 1
+              var tranformVal = (count );
+              tranformVal -= (tranformVal / (tranformVal*10)) ;     
+              $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+(tranformVal)+")"}); 
+                                                      }
+            
+
+
+ 
+
 
 
   });
-
-
-
-
-
-  $(".scroll-container").on("mouseenter", function() {
-  var lastScrollTop = 0;
-  var count =0;
 
   
-    $(window).scroll(function () {
-        var windowTop = $(window).scrollTop();  
-    
 
 
-  var st = $(this).scrollTop();
-   if (st > lastScrollTop){
-      console.log(count += 1);
-         $(".scroll-container .circle-wrapper .circle-radius ").addClass('add-circle');
-          $(".scroll-container .circle-wrapper .circle-radius").css("transform","scale("+count+")");
-   } else {
- console.log(count -=1);
-   }
-   lastScrollTop = st;
-
-
-    
-  });
-
-
-
-
-
-  }).on("mouseleave", function() {
-
-    
-    return false;
-   });
-
-
-
-
-});
+ });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'js/particles.json', function () {
   // console.log('callback - particles.js config loaded');
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js2', 'js/particles.json', function () {
+  // console.log('callback - particles.js config loaded');
+});
+
+
 
 
 
@@ -104,3 +110,124 @@ function particle_parrallax() {
 
 
 
+
+
+
+
+
+
+// muzamil
+
+    //  if(positiondown < 1  &&    (positivePositon < 612)  ){
+    
+    //     // $(".scroll-container").addClass("fixer");
+        
+    //        count += 1
+    //       tranformVal = (count * 120);
+    //       $(".atom-feature ").css('height', tranformVal);
+    //       // $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+tranformVal+")"});
+    //          $(".scroll-container .circle-wrapper .circle-radius").css({"width":tranformVal,"height":tranformVal,
+    //          "left":((-tranformVal)/2),"top":((-tranformVal)/2)});
+
+        
+
+    //   }
+           
+      
+     
+  
+    //   else{
+    //     $(".scroll-container").removeClass("fixer")
+    //   }
+
+
+
+    
+// $('.scroll-container').on('scroll' , function(){
+//   console.log('abc');
+// });
+
+
+
+  // $(".scroll-container").on("mouseenter", function() {
+  // var lastScrollTop = 0;
+  // var count =0,tranformVal;
+
+  
+  //   $(window).scroll(function () {
+  //       var windowTop = $(window).scrollTop();  
+    
+
+
+  // var st = $(this).scrollTop();
+  //  if (st > lastScrollTop){
+     
+  //     count += 1
+  //     tranformVal = (count /100);
+  //        $(".scroll-container .circle-wrapper .circle-radius ").addClass('add-circle');
+  //         $(".scroll-container .circle-wrapper .circle-radius").css("transform","scale("+tranformVal+")");
+  //  } else {
+  //   count -=1;
+  //  }
+  //  lastScrollTop = st;
+
+
+    
+  // });
+
+
+
+
+
+//   }).on("mouseleave", function() {
+
+    
+//     return false;
+//    });
+
+
+
+
+
+  // if(positiondown < 1  &&    (positivePositon < 612)  ){
+    
+  //       // $(".scroll-container").addClass("fixer");
+        
+  //          count += 1
+  //         tranformVal = (count * 120);
+  //         $(".atom-feature ").css('height', tranformVal);
+  //         // $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+tranformVal+")"});
+  //            $(".scroll-container .circle-wrapper .circle-radius").css({"width":tranformVal,"height":tranformVal,
+  //            "left":((-tranformVal)/2),"top":((-tranformVal)/2)});
+
+        
+
+  //     }
+           
+      
+     
+  
+  //     else{
+  //       $(".scroll-container").removeClass("fixer")
+  //     }
+
+
+
+
+
+// translate
+
+      //  $("nav").css({ "transform":"translateY("+((scrollTop)) +"px)"});
+
+    //console.log(scrollTop);
+   // console.log(scroll_circle - $(window).scrollTop() - headerHeight);
+    // var positiondown = (scroll_circle - scrollTop - headerHeight);
+ 
+    //   if( scrollTop >= (1000)){
+    //     $(".wrapper-holder").css({"height":+wraper_height , "transform":"translate(0px,"+ (-(1000)) +"px)"});
+  
+    //   }
+    //   else{
+    //     $(".wrapper-holder").css({"height":+wraper_height , "transform":"translate(0px,"+ (-(scrollTop)) +"px)"});
+     
+    //   }
