d0592903e49495e7856b53a8058b8a6e5638bacc
naveednasib <naveed.nasib@gaditek.com>
2017-08-03 22:39:56 +0500

    final changes with scroll fix

--- js/homepage.js
+++ js/homepage.js
@@ -2,62 +2,26 @@
 
 $(document).ready(function () {
 
+
+  
   var options = {
     strings: ["Hardware &amp; Software Manufacturers..", "Businesses.", "Organizations (For or Not For Profit)..", "Govermental Agencies.."],
     typeSpeed: 70,
     loop: true
   }
-
   var typed = new Typed(".element", options);
 
 
-    var scroll_circle = $(".scroll-container").offset().top,
-    headerHeight = $('header').height();
-    var scroll_circle_height = $(".scroll-container").height();
-    var  scroll_circle_bottom = $(".scroll-container").offset().top;
-    scroll_circle_bottom = scroll_circle_height + scroll_circle ;
+ 
 
-    circleScrolled = 0;
-    lastScroll = 0;
-    var count =0;
-    var wraper_height =  $(".wrapper-holder").height();
-    var tranformVal;
+    
+ 
 
 
 
   $(window).scroll(function (e) {
     particle_parrallax();
-    scrollTop = $(window).scrollTop();
-
-
     
-    
-       var positiondown = (scroll_circle - scrollTop - headerHeight);      
-       var positivePositon = (-(positiondown)   );
-       
-      if(positiondown < 1   &&    (positivePositon < scroll_circle_height)  ){          
-          $(".circle-radius").addClass("add-circle");
-            $(".atom-feature").css("margin-bottom",positivePositon);
-            count += 1;           
-           var tranformVal = (count );
-           tranformVal += (tranformVal / (tranformVal*10)) ;
-            $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+(tranformVal)+")"});
-           }    
-
-      
-          else if(scroll_circle_bottom <=  scrollTop){          
-              console.log(scroll_circle_bottom  +" __ "+  scrollTop);              
-              count -= 1
-              var tranformVal = (count );
-              tranformVal -= (tranformVal / (tranformVal*10)) ;     
-              $(".scroll-container .circle-wrapper .circle-radius").css({"transform":"scale("+(tranformVal)+")"}); 
-                                                      }
-            
-
-
- 
-
-
 
   });
 
@@ -66,14 +30,14 @@ $(document).ready(function () {
 
  });
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
 particlesJS.load('particles-js', 'js/particles.json', function () {
-  // console.log('callback - particles.js config loaded');
+
 });
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
 particlesJS.load('particles-js2', 'js/particles.json', function () {
-  // console.log('callback - particles.js config loaded');
+
 });
 
 
