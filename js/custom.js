0633805d59955a2c582e9fb9ef1fbe2562af8d76
Ahmer Jamil <ahmer.jamil@gaditek.com>
2017-08-08 22:17:53 +0500

    Atom Changes

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    /*dropdown menu active */
+    $("ul.dropdown-menu.atom-menu li.menu-item").hover(function () {
+        $("ul.dropdown-menu.atom-menu li.menu-item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    /* Header Background Color On scroll*/
+    if ($(document).scrollTop() > 20) {
+
+        $("header").addClass('bg-color');
+    } else {
+        $("header").removeClass('bg-color');
+    }
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 20) {
+            $("header").addClass('bg-color');
+        } else {
+            $("header").removeClass('bg-color');
+        }
+        /* atom-for circle scattering */
+        var $windowTop = $(document).scrollTop();
+        var $elementTop = $(".atom-for").offset().top;
+        if ($windowTop >= $elementTop) {
+            $(".scattered-circles").addClass("animated bounceIn");
+            setTimeout(function () {
+                $(".circle1").addClass("active");
+            }, 1300);
+
+        }
+    });
+    /*circle-text active */
+    $(".circle-wrapper").hover(function () {
+        $(".circle1").removeClass('active');
+
+    });
+    /*footer list active */
+    $(".footer-list li").click(function () {
+        $(".footer-list li").removeClass("active");
+        $(this).addClass("active");
+    });
+    /*Scroll-contianer tile active */
+    $(".scroll-container .tile-container .tile").hover(function () {
+        /*Scroll-contianer tile active */
+        $(".scroll-container .tile-container .tile").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    /*slider-partial-container os-type active */
+    $(".slider-partial-container .os-types li").hover(function () {
+        $(".slider-partial-container .os-types li").removeClass("active");
+        $(this).addClass("active");
+
+    });
+    /*setting box dimensions equal to image dimensions */
+    var img = $(".slider-partial-container .img-container img");
+    $(".slider-partial-container .img-container").css({height:img.height()});
+
+    /*Changing image on click*/
+    $(".slider-partial-container .os-types li").click(function () {
+        $('.slider-partial-container .img-container .img-btn-wrapper').siblings().removeClass('active');
+        $('.slider-partial-container .img-container .img-btn-wrapper').eq($(this).index()).addClass('active');
+    });
+});
--- js/homepage.js
+++ js/homepage.js
@@ -1,63 +1,19 @@
 $(document).ready(function () {
 
 
-
-  var options = {
-    strings: ["Hardware &amp; Software Manufacturers..", "Businesses.", "Organizations (For or Not For Profit)..", "Govermental Agencies.."],
-    typeSpeed: 70,
-    loop: true
-  }
-  var typed = new Typed(".element", options);
-
-
-
-
-
-
-
-
-
-  $(window).scroll(function (e) {
-    particle_parrallax();
-
-
-  });
-
-
-  /* Header Background Color On scroll*/
-    if ($(document).scrollTop() > 20) {
-        $("header").addClass('bg-color');
-    } else {
-        $("header").removeClass('bg-color');
+    var options = {
+        strings: ["Hardware &amp; Software Manufacturers..", "Businesses.", "Organizations (For or Not For Profit)..", "Govermental Agencies.."],
+        typeSpeed: 70,
+        loop: true
     }
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 20) {
-            $("header").addClass('bg-color');
-        } else {
-            $("header").removeClass('bg-color');
-        }
-      /* atom-for circle scattering */
-        var $windowTop = $(document).scrollTop();
-        var $elementTop = $(".atom-for").offset().top;
-        if ($windowTop >= $elementTop) {
-            $(".scattered-circles").addClass("animated bounceIn");
-            setTimeout(function () {
-                $(".circle1").addClass("active");
-            }, 1300);
+    var typed = new Typed(".element", options);
 
-        }
-    });
-  /*circle-text active */
-    $(".circle-wrapper").hover(function () {
-        $(".circle1").removeClass('active');
+
+    $(window).scroll(function (e) {
+        particle_parrallax();
+
 
     });
-  /*footer list active */
-    $(".footer-list li").click(function () {
-        $(".footer-list li").removeClass("active");
-        $(this).addClass("active");
-    });
-
 
 
 });
@@ -68,15 +24,9 @@ particlesJS.load('particles-js', 'js/particles.json', function () {
 });
 
 
-particlesJS.load('particles-js2', 'js/particles.json', function () {
+particlesJS.load('particles-js2', 'js/particles2.json', function () {
 
 });
-
-
-
-
-
-
 
 
 var addingRatio;
@@ -84,22 +34,22 @@ var CurrentScroll = 0;
 
 function particle_parrallax() {
 
-  var particleSvg = $(".paralla-abs-svg");
-  particleSvg = parseInt(particleSvg.css("top"));
-  var windowTop = $(window).scrollTop();
-  var hardwareTop = $(".hardware-section").offset().top;
-  addingRatio = ((windowTop + windowTop) / windowTop);
-  var NextScroll = $(this).scrollTop();
-  if (NextScroll > CurrentScroll) {
-    //write the codes related to down-ward scrolling here
-    particleSvg += addingRatio;
-  }
-  else {
-    //write the codes related to upward-scrolling here
-    particleSvg -= addingRatio;
-  }
-  $(".paralla-abs-svg").css("top", particleSvg);
-  CurrentScroll = NextScroll;  //Updates current scroll position
+    var particleSvg = $(".paralla-abs-svg");
+    particleSvg = parseInt(particleSvg.css("top"));
+    var windowTop = $(window).scrollTop();
+    var hardwareTop = $(".hardware-section").offset().top;
+    addingRatio = ((windowTop + windowTop) / windowTop);
+    var NextScroll = $(this).scrollTop();
+    if (NextScroll > CurrentScroll) {
+        //write the codes related to down-ward scrolling here
+        particleSvg += addingRatio;
+    }
+    else {
+        //write the codes related to upward-scrolling here
+        particleSvg -= addingRatio;
+    }
+    $(".paralla-abs-svg").css("top", particleSvg);
+    CurrentScroll = NextScroll;  //Updates current scroll position
 
 
 }
